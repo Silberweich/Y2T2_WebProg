25e1f3d141194f42e3448b7457f22a7abef5f22c
Vvidsky <kulawutm.developer@gmail.com>
2022-04-02 21:38:17 +0700

    Added comment auth.js

--- auth.js
+++ auth.js
@@ -3,7 +3,7 @@ const jwt = require("jsonwebtoken");
 module.exports = (req, res, next) => {
     try{
         // const token = req.headers.authorization.split(" ")[1];
-        // Getting token token from the cookie
+        // Getting token token from the cookie (require cookie-parser to do so)
         const token = req.cookies.token;
         var decoded = jwt.verify(token, process.env.SECRET);
         console.log(decoded);
