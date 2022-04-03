637e79c8dd3be1c4bcb387c237ef7e2df75245a7
Silberweich <69336541+Silberweich@users.noreply.github.com>
2022-04-03 14:28:54 +0700

    mohterfukeru

--- index.js
+++ index.js
@@ -310,5 +310,5 @@ app.use((req, res, next) => {
     res.status(404).send("NONO, GO BACK, BAD");
 })
 
-console.log("Listening on the port 3030");
-app.listen(3030); 
+console.log("Listening on the port 4203");
+app.listen(4203); 
