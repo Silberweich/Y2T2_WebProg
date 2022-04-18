3989ab546341e580833008d84238057029cfaff5
Kulawut Makkamoltham <81841604+Vvidsky@users.noreply.github.com>
2022-04-18 17:39:16 +0700

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,27 +1,5 @@
 import logo from './logo.svg';
 import './App.css';
-<<<<<<< HEAD
-
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-=======
 import React from 'react';
 
 class Friends extends React.Component {
@@ -169,7 +147,6 @@ class App extends React.Component {
       </div>
     );
   }
->>>>>>> 20fedff4a7b013411c675a39fa9fa7142d970536
 }
 
 export default App;
